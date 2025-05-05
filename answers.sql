b8282490583ccdb5b73e40adfad3ca417282d1e4
spookeylucy <lucyannmwangi141@gmail.com>
2025-05-05 22:06:25 +0300

    correct answers

--- answers.sql
+++ answers.sql
@@ -1,36 +1,41 @@
--- Creating a 1NF version of ProductDetail
-SELECT 
-    OrderID,
-    CustomerName,
-    Product
-FROM 
-    (
-    VALUES 
-        (101, 'John Doe', 'Laptop'),
-        (101, 'John Doe', 'Mouse'),
-        (102, 'Jane Smith', 'Tablet'),
-        (102, 'Jane Smith', 'Keyboard'),
-        (102, 'Jane Smith', 'Mouse'),
-        (103, 'Emily Clark', 'Phone')
-    ) AS ProductDetail(OrderID, CustomerName, Product);
+ --  Question 1
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+-- Question 2
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
 
 
-
-
-
-
--- Table for customers (no partial dependency)
-SELECT DISTINCT
-    OrderID,
-    CustomerName
-FROM 
-    OrderDetails;
-
-
--- Table for the products ordered
-SELECT 
-    OrderID,
-    Product,
-    Quantity
-FROM 
-    OrderDetails;
+CREATE TABLE Product (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
